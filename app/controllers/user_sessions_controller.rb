6df57ec18930be235acd1227ff34bc0de221caee
HiroshiIshida <hishida@cuon.co.jp>
2016-09-27 19:28:55 +0900

    [#128844525] Use google strage when production mode

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -8,14 +8,16 @@ class UserSessionsController < ApplicationController
     @session = UserSession.new(user)
     @session.save!
     redirect_to project_list_path, flash: { success: t('action.login.success') }
-  rescue
+  rescue => e
+    logger.error e.message
     redirect_to root_path, flash: { error: t('action.login.fail') }
   end
 
   def destroy
     current_user_session.destroy
     redirect_to root_path, flash: { success: t('action.logout.success') }
-  rescue
+  rescue => e
+    logger.error e.message
     redirect_to root_path, flash: { error: t('action.logout.fail') }
   end
 end
