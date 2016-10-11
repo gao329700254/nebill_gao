4be7e758a45939bb5d09cff372ffa5c575c1f27b
HiroshiIshida <hishida@cuon.co.jp>
2016-10-11 21:23:05 +0900

    Fix carrierwave bytecode error

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,11 +1,13 @@
 if Rails.env.production?
+  CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/
   CarrierWave.configure do |config|
+    config.root = Rails.root.join('tmp')
     config.fog_provider = 'fog/google'
     config.fog_credentials = {
       provider:                         'Google',
       google_storage_access_key_id:     Rails.application.secrets.google_storage_access_key_id,
       google_storage_secret_access_key: Rails.application.secrets.google_storage_secret_access_key,
     }
-    config.fog_directory = 'nebill_production'
+    config.fog_directory = 'nebill-production'
   end
 end
