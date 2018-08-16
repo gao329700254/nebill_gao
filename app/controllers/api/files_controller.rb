6b6f57909641a59cbd22b5e86d49bbec66f6f25d
cuonyohno <yohno@cuon.co.jp>
2018-08-16 14:04:03 +0900

    稟議のファイルダウンロード処理で、fog経由だとファイルが無いのを修正

--- app/controllers/api/files_controller.rb
+++ app/controllers/api/files_controller.rb
@@ -1,14 +1,10 @@
 class Api::FilesController < Api::ApiController
   def approval_file_download
-    @file = ApprovalFile.find(params[:files_id].to_i)
-    file_download
-  end
+    @file = ApprovalFile.find(params[:files_id])
+    
+    @file.file.cache_stored_file!
+    @file.file.retrieve_from_cache!(@file.file.cache_name)
 
-private
-
-  def file_download
-    filepath = @file.file.current_path
-    stat = File.stat(filepath)
-    send_file(filepath, filename: @file.original_filename, length: stat.size)
+    send_data @file.file.read, filename: @file.original_filename, length: @file.file.size
   end
 end
