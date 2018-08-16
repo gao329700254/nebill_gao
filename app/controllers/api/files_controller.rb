cfad16ac46db81faea91119fe12088b383e0274c
cuonyohno <yohno@cuon.co.jp>
2018-08-16 16:13:39 +0900

    Revert "稟議のファイルダウンロード処理で、fog経由だとファイルが無いのを修正"
    This reverts commit c801e6aa07050fad105f226cd1ddc72fef148892.

--- app/controllers/api/files_controller.rb
+++ app/controllers/api/files_controller.rb
@@ -1,10 +1,14 @@
 class Api::FilesController < Api::ApiController
   def approval_file_download
-    @file = ApprovalFile.find(params[:files_id])
+    @file = ApprovalFile.find(params[:files_id].to_i)
+    file_download
+  end
 
-    @file.file.cache_stored_file!
-    @file.file.retrieve_from_cache!(@file.file.cache_name)
+private
 
-    send_file @file.file.current_path, filename: @file.original_filename, length: @file.file.size
+  def file_download
+    filepath = @file.file.current_path
+    stat = File.stat(filepath)
+    send_file(filepath, filename: @file.original_filename, length: stat.size)
   end
 end
