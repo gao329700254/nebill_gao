faf904e5023b9c5670476fff50f9b274fcf4dfed
yoshinaka wataru <wyoshinaka@cuon.co.jp>
2019-02-28 10:52:53 +0900

    s3対応

--- app/controllers/api/files_controller.rb
+++ app/controllers/api/files_controller.rb
@@ -3,7 +3,7 @@ class Api::FilesController < Api::ApiController
     @apprival_file = ApprovalFile.find(params[:files_id])
     file = @apprival_file.file
 
-    file.download!(file.url) if Rails.env.production?
+    file.download!(file.url) if ENV["FILE_STORAGE_TYPE"] == "fog"
     send_file file.path, filename: @apprival_file.original_filename
   end
 
@@ -11,7 +11,7 @@ class Api::FilesController < Api::ApiController
     @expense_file = ExpenseFile.find(params[:files_id])
     file = @expense_file.file
 
-    file.download!(file.url) if Rails.env.production?
+    file.download!(file.url) if ENV["FILE_STORAGE_TYPE"] == "fog"
     send_file file.path, filename: @expense_file.original_filename
   end
 end
