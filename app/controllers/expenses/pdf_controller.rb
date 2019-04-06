b6bdc438012989cc36b7eda735091f9272c8a1c1
Mototsugu Takahama <mtakahama@cuon.co.jp>
2019-04-07 03:19:38 +0900

    divide expsen pdf

--- app/controllers/expenses/pdf_controller.rb
+++ app/controllers/expenses/pdf_controller.rb
@@ -14,6 +14,9 @@ private
   def set_expense
     if params[:id] != '0'
       @expense_approval = ExpenseApproval.find(params[:id])
+      expenses          = @expense_approval.expense.eager_load(:default).order(:use_date)
+      @routing_expenses = expenses.merge(DefaultExpenseItem.where(is_routing: true))
+      @other_expenses   = expenses.merge(DefaultExpenseItem.where(is_routing: false))
     else
       flash[:error] = I18n.t("errors.messages.expense_approval_is_empty")
       redirect_to(:back)
