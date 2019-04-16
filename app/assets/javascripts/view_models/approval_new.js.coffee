0904b12c68cf5f41e312a2cfd4fdff326447ac90
cuonyohno <yohno@cuon.co.jp>
2019-04-16 12:24:47 +0900

    lintの警告を修正

--- app/assets/javascripts/view_models/approval_new.js.coffee
+++ app/assets/javascripts/view_models/approval_new.js.coffee
@@ -1,17 +1,17 @@
 $ ->
   window.approvalShow = new Vue
     el: '#approval_new'
-    ready: () ->
+    ready: ->
       # nested_fields_forの中ではvueJSのeventbindingが使えないため、ここで無理やりイベントを発行するようにしています。
-      $(@$el).on('dragleave', '.approval_new__form__item__file__drop_container',(e)=>
+      $(@$el).on('dragleave', '.approval_new__form__item__file__drop_container', (e) =>
         e.preventDefault()
         @leave(e)
       )
-      $(@$el).on('dragover', '.approval_new__form__item__file__drop_container',(e)=>
+      $(@$el).on('dragover', '.approval_new__form__item__file__drop_container', (e) =>
         e.preventDefault()
         @over(e)
       )
-      $(@$el).on('drop', '.approval_new__form__item__file__drop_container',(e)=>
+      $(@$el).on('drop', '.approval_new__form__item__file__drop_container', (e) =>
         e.preventDefault()
         @onDrop(e)
       )
--- app/services/chatwork/file_upload_service.rb
+++ app/services/chatwork/file_upload_service.rb
@@ -1,7 +1,7 @@
 class Chatwork::FileUploadService < BaseService
   attr_accessor :params
 
-  UPLOAD_FILE_REGEX = %r(\[download:(?<download_id>\d*)\].*?\[/download\])
+  UPLOAD_FILE_REGEX = %r{\[download:(?<download_id>\d*)\].*?\[/download\]}
 
   def initialize(params)
     self.params = params
@@ -9,20 +9,21 @@ class Chatwork::FileUploadService < BaseService
 
   def execute
     if upload_user.blank?
-      logger.info "user not exists chatwork_id: #{webhook_params[:from_account_id]}" and return
+      logger.info("user not exists chatwork_id: #{webhook_params[:from_account_id]}") && return
     end
 
     create_expense!
   end
 
 private
+
   def webhook_params
     @webhook_params ||= begin
       params.require(:webhook_event).tap do |webhook_params|
-        %i[from_account_id room_id body].each { |att| webhook_params.require(att) }
+        %i(from_account_id room_id body).each { |att| webhook_params.require(att) }
       end
 
-      params.require(:webhook_event).permit(%i[from_account_id room_id body]).tap do |webhook_params|
+      params.require(:webhook_event).permit(%i(from_account_id room_id body)).tap do |webhook_params|
         webhook_params[:download_id] = webhook_params[:body].match(UPLOAD_FILE_REGEX)[:download_id]
       end
     end
@@ -37,7 +38,7 @@ private
   end
 
   def create_expense!
-    expense = upload_user.expenses.build(use_date: Date.today, default: default_item, amount: default_item.standard_amount || 100)
+    expense = upload_user.expenses.build(use_date: Time.zone.today, default: default_item, amount: default_item.standard_amount || 100)
 
     expense.file.build(attch_uploaded_file_params)
 
@@ -47,6 +48,6 @@ private
   def attch_uploaded_file_params
     res = Chatwork::Base.get("v2/rooms/#{webhook_params[:room_id]}/files/#{webhook_params[:download_id]}", create_download_url: 1)
 
-    {file: open(res['download_url']), original_filename: res['filename']}
+    { file: open(res['download_url']), original_filename: res['filename'] }
   end
 end
