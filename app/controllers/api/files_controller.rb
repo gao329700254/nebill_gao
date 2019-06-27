06e4f0873996f80ca4d33dc9be8519e5be4a1e9d
Yuhao Gao <uko@cuon.co.jp>
2019-06-27 10:58:45 +0900

    refactoring

--- app/controllers/api/files_controller.rb
+++ app/controllers/api/files_controller.rb
@@ -1,34 +1,26 @@
 class Api::FilesController < Api::ApiController
   def approval_file_download
-    @approval_file = ApprovalFile.find(params[:files_id])
-    file = @approval_file.file
-
-    if ENV["FILE_STORAGE_TYPE"] == "fog"
-      data = open(file.url)
-      send_data data.read, filename: @approval_file.original_filename
-    end
-    send_file file.path, filename: @approval_file.original_filename
+    file_download(ApprovalFile.find(params[:files_id]))
   end
 
   def expense_file_download
-    @expense_file = ExpenseFile.find(params[:files_id])
-    file = @expense_file.file
-
-    if ENV["FILE_STORAGE_TYPE"] == "fog"
-      data = open(file.url)
-      send_data data.read, filename: @approval_file.original_filename
-    end
-    send_file file.path, filename: @expense_file.original_filename
+    file_download(ExpenseFile.find(params[:files_id]))
   end
 
   def client_file_download
-    @client_file = ClientFile.find(params[:files_id])
-    file = @client_file.file
+    file_download(ClientFile.find(params[:files_id]))
+  end
+
+  private
+
+  def file_download(file_model)
+    file = file_model.file
 
     if ENV["FILE_STORAGE_TYPE"] == "fog"
       data = open(file.url)
-      send_data data.read, filename: @approval_file.original_filename
+      send_data data.read, filename: file_model.original_filename
+    else
+      send_file file.path, filename: file_model.original_filename
     end
-    send_file file.path, filename: @client_file.original_filename
   end
 end
