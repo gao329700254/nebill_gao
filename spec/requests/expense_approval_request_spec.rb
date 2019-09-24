e4673a6ffd23105ba04f11891695b5c26816a437
Kyohei Maeda <kmaeda@cuon.co.jp>
2019-09-25 08:38:26 +0900

    スペックの修正

--- spec/requests/expense_approval_request_spec.rb
+++ spec/requests/expense_approval_request_spec.rb
@@ -30,55 +30,37 @@ RSpec.describe ExpenseApproval, type: :request do
   end
 
   describe 'PATCH /api/expense_approvals/:id' do
-    subject do
-      patch path, params: params
-      expense_approval.reload
+    subject { patch path, params: params }
+
+    let(:applicant)        { create(:user) }
+    let(:admin_user)       { create(:admin_user) }
+    let(:expense_approval) { create(:expense_approval, created_user: applicant, status: "permission") }
+    let(:path)             { "/api/expense_approvals/#{expense_approval.id}" }
+    let(:params) do
+      {
+        id:     expense_approval.id,
+        button: 'disconfirm',
+        expense_approval_user: { comment: '' },
+      }
     end
 
-    context 'updated disconfirm application by applicant' do
-      let(:applicant)              { create(:user) }
-      let(:approver)               { create(:user) }
-      let(:admin_user)             { create(:admin_user) }
-      let!(:expense_approval)      { create(:expense_approval, created_user: applicant, status: "permission") }
-      let!(:expense_approval_user) { create(:expense_approval_user, user: approver, expense_approval: expense_approval) }
-      let(:cw_instance)            { Chatwork::ExpenseApproval.new }
-      let(:path)                   { "/api/expense_approvals/#{expense_approval.id}" }
-      let(:params) do
-        {
-          id:     expense_approval.id,
-          button: 'disconfirm',
-          expense_approval_user: { comment: "" },
-        }
-      end
-
-      it 'applicant cannot disconfirm "permission" expense approval' do
-        login(applicant)
-        expect { subject }.to raise_error(NoMethodError)
-        expect(expense_approval.status).to eq 'permission'
-      end
-
-      it 'admin user cannot disconfirm "permission" expense approval' do
-        login(admin_user)
-        expect { subject }.to raise_error(NoMethodError)
-        expect(expense_approval.status).to eq 'permission'
-      end
+    example 'applicant cannot disconfirm "permission" expense approval' do
+      login(applicant)
+      expect { subject }.to raise_error(NoMethodError)
+      expect(expense_approval.reload.status).to eq 'permission'
     end
 
-    context 'updated disconfirm application by approver' do
-      let(:applicant)              { create(:user) }
+    example 'admin user cannot disconfirm "permission" expense approval' do
+      login(admin_user)
+      expect { subject }.to raise_error(NoMethodError)
+      expect(expense_approval.reload.status).to eq 'permission'
+    end
+
+    context 'current user is approver' do
       let(:approver)               { create(:user) }
-      let!(:expense_approval)      { create(:expense_approval, created_user: applicant, status: "permission") }
       let!(:expense_approval_user) { create(:expense_approval_user, user: approver, expense_approval: expense_approval) }
       let(:mailer)                 { ExpenseApprovalMailer.disconfirm_expense_approval }
       let(:cw_instance)            { Chatwork::ExpenseApproval.new }
-      let(:path)                   { "/api/expense_approvals/#{expense_approval.id}" }
-      let(:params) do
-        {
-          id:     expense_approval.id,
-          button: 'disconfirm',
-          expense_approval_user: { comment: "" },
-        }
-      end
 
       before do
         allow(ExpenseApprovalMailer).to receive(:disconfirm_expense_approval).and_return(mailer)
@@ -88,9 +70,9 @@ RSpec.describe ExpenseApproval, type: :request do
         login(approver)
       end
 
-      it ' can disconfirm "permission" expense approval' do
+      it 'can disconfirm "permission" expense approval' do
         subject
-        expect(expense_approval.status).to eq 'disconfirm'
+        expect(expense_approval.reload.status).to eq 'disconfirm'
       end
     end
   end
--- spec/requests/expense_request_spec.rb
+++ spec/requests/expense_request_spec.rb
@@ -3,19 +3,15 @@ require 'rails_helper'
 RSpec.describe Expense, type: :request do
   describe 'POST /api/expenses/reapproval.json' do
     context 'reapply disconfirm application by applicant' do
-      let(:applicant)             { create(:user) }
-      let(:approver)              { create(:user) }
-      let(:expense)               { create(:expense, expense_approval: expense_approval) }
-      let(:expense_approval)      { create(:expense_approval, created_user: applicant, status: "disconfirm") }
+      let(:applicant)              { create(:user) }
+      let(:approver)               { create(:user) }
+      let(:expense)                { create(:expense, expense_approval: expense_approval) }
+      let(:expense_approval)       { create(:expense_approval, created_user: applicant, status: "disconfirm") }
       let!(:expense_approval_user) { create(:expense_approval_user, user: approver, expense_approval: expense_approval) }
-      let(:mailer)                { ExpenseApprovalMailer.disconfirm_expense_approval }
-      let(:cw_instance)           { Chatwork::ExpenseApproval.new }
-      let(:path)                  { "/api/expenses/reapproval.json" }
-      let(:params) do
-        {
-          selectedApproval: expense_approval.id,
-        }
-      end
+      let(:mailer)                 { ExpenseApprovalMailer.disconfirm_expense_approval }
+      let(:cw_instance)            { Chatwork::ExpenseApproval.new }
+      let(:path)                   { '/api/expenses/reapproval' }
+      let(:params)                 { { selectedApproval: expense_approval.id } }
 
       before do
         allow(ExpenseApprovalMailer).to receive(:update_expense_approval).and_return(mailer)
@@ -23,13 +19,11 @@ RSpec.describe Expense, type: :request do
         allow(Chatwork::ExpenseApproval.new).to receive(:expense_approval).and_return(:cw_instance)
         allow(cw_instance).to receive(:notify_disconfirm).and_return(true)
         login(applicant)
+        post path, params: params
       end
 
       it 'can reapply "disconfirm" expense approval' do
-        post path, params: params
-        expense_approval.reload
-
-        expect(expense_approval.status).to eq "pending"
+        expect(expense_approval.reload.status).to eq "pending"
       end
     end
   end
