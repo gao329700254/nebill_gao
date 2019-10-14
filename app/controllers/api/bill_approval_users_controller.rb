c8c68dedb18cc0cbf3af48fcea4be717f55112de
Kyohei Maeda <kmaeda@cuon.co.jp>
2019-10-14 14:51:28 +0900

    例外処理を追加、コメントを追記

--- app/controllers/api/bill_approval_users_controller.rb
+++ app/controllers/api/bill_approval_users_controller.rb
@@ -1,34 +1,41 @@
 class Api::BillApprovalUsersController < Api::ApiController
+  # 承認者による「承認」アクション
   def create
-    @bill = Bill.find(params[:bill_id])
-    @bill.approved_bill!
-
+    @bill             = Bill.find(params[:bill_id])
     @current_approver = @bill.bill_approval_users.where(user_id: @current_user.id).first
-    @current_approver.update(status: 'approved', comment: params[:comment])
 
-    show_success_message(@bill, :approve, params[:bill_id])
-  rescue ActiveRecord::RecordInvalid
-    show_failure_message(@bill, :approve)
+    ActiveRecord::Base.transaction do
+      @bill.approved_bill!
+      @current_approver.update!(status: 'approved', comment: params[:comment])
+
+      show_success_message(params[:bill_id], @bill, :approve)
+    rescue ActiveRecord::RecordInvalid
+      show_failure_message(:approve)
+    end
   end
 
+  # 承認者による「差戻」アクション
+  # 差し戻された時、全承認者のステータスを「差戻」にする
+  # 差し戻したユーザについては、コメント付きで更新する
   def update
-    bill_id = params[:bill_approval_user][:bill_id]
-    @bill   = Bill.find(bill_id)
-    @bill.sent_back_bill!
-
+    bill_id           = params[:bill_approval_user][:bill_id]
+    @bill             = Bill.find(bill_id)
     @current_approver = @bill.bill_approval_users.where(user_id: @current_user.id)
-    @current_approver.update(status: 'sent_back', comment: params[:bill_approval_user][:comment])
 
-    @bill.bill_approval_users.each(&:sent_back_bill!)
+    ActiveRecord::Base.transaction do
+      @bill.sent_back_bill!
+      @current_approver.update!(status: 'sent_back', comment: params[:bill_approval_user][:comment])
+      @bill.bill_approval_users.update!(status: 'sent_back')
 
-    show_success_message(@bill, :send_back, bill_id)
-  rescue ActiveRecord::RecordInvalid
-    show_failure_message(@bill, :send_back)
+      show_success_message(bill_id, @bill, :send_back)
+    rescue ActiveRecord::RecordInvalid
+      show_failure_message(:send_back)
+    end
   end
 
 private
 
-  def show_success_message(model, action, params)
+  def show_success_message(params, model, action)
     redirect_to bill_show_path(params)
     action_model_flash_success_message(model, action)
   end
--- app/controllers/bill_applicants_controller.rb
+++ app/controllers/bill_applicants_controller.rb
@@ -1,41 +1,36 @@
 class BillApplicantsController < ApplicationController
   # 申請者による、「申請」アクション
+  # 申請時に承認者を設定する
   def create
     @bill = Bill.find(params[:bill_id])
 
-    @bill.pending_bill!
-    @bill.bill_applicant.update!(comment: params[:comment])
+    ActiveRecord::Base.transaction do
+      @bill.pending_bill!
+      @bill.bill_applicant.update!(comment: params[:comment])
 
-    create_bill_approval_users!
+      create_bill_approval_users!
 
-    show_success_message(@bill, :apply, params[:bill_id])
-  rescue ActiveRecord::RecordInvalid
-    show_failure_message(:apply)
+      show_success_message(@bill, :apply, params[:bill_id])
+    rescue ActiveRecord::RecordInvalid
+      show_failure_message(:apply)
+    end
   end
 
-  # 申請者による、「取り消し」アクション
+  # 申請者による、「取消」アクション
+  # 取消した時、未申請時と同じように、修正・承認者選択ができるようにする
+  # 申請時に改めて承認者を選び直すため、取消時に設定済みの承認者を削除する
   def update
     bill_id = params[:bill_applicant][:bill_id]
     @bill   = Bill.find(bill_id)
 
-    @bill.cancelled_bill!
-    @bill.bill_approval_users.destroy_all
+    ActiveRecord::Base.transaction do
+      @bill.cancelled_bill!
+      @bill.bill_approval_users.destroy_all
 
-    show_success_message(@bill, :cancel, bill_id)
-  rescue ActiveRecord::RecordInvalid
-    show_failure_message(:cancel)
-  end
-
-private
-
-  def show_success_message(model, action, params)
-    flash[:success] = I18n.t("action.#{action}.success", model: I18n.t("activerecord.models.#{model.class.name.underscore}"))
-    redirect_to bill_show_path(params)
-  end
-
-  def show_failure_message(action)
-    flash[:error] = I18n.t("action.#{action}.fail", model: I18n.t('activerecord.models.bill'))
-    redirect_to(:back)
+      show_success_message(@bill, :cancel, bill_id)
+    rescue ActiveRecord::RecordInvalid
+      show_failure_message(:cancel)
+    end
   end
 
 private
@@ -48,5 +43,15 @@ private
     # 「社長フラグ(= is_chief)」を有するユーザを二段目承認者として作成する
     chief = User.find_by(is_chief: true)
     @bill.bill_approval_users.create!(role: 'secondary', status: 'pending', user_id: chief.id)
+  end
+
+  def show_success_message(model, action, params)
+    flash[:success] = I18n.t("action.#{action}.success", model: I18n.t("activerecord.models.#{model.class.name.underscore}"))
+    redirect_to bill_show_path(params)
+  end
+
+  def show_failure_message(action)
+    flash[:error] = I18n.t("action.#{action}.fail", model: I18n.t('activerecord.models.bill'))
+    redirect_to(:back)
   end
 end
