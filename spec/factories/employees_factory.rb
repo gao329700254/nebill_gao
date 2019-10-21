dd845059905eb62e074e16faba09ddecdf9dc04e
佐川　愛 <asagawa@cuon.co.jp>
2019-10-21 19:28:14 +0900

    rubocop修正6

--- spec/factories/employees_factory.rb
+++ spec/factories/employees_factory.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :employee do
-    sequence(:actable_id){ |n| n }
-    sequence(:actable_type){ 'User' }
+    sequence(:actable_id) { |n| n }
+    sequence(:actable_type) { 'User' }
     name { Faker::Name.name }
     email { Faker::Internet.email }
   end
