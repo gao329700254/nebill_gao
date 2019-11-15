9a13d2ed1dd0d916b092a4c4d61ca31c125ed78d
Yu Utsugi <yutsugi@cuon.co.jp>
2019-11-15 17:26:10 +0900

    fix

--- spec/models/approval_spec.rb
+++ spec/models/approval_spec.rb
@@ -6,8 +6,6 @@ RSpec.describe Approval, type: :model do
     let(:other_user)  { create(:user) }
     let(:approval1)   { create(:approval, :user_approval, created_user: current_user, created_at: "2019-10-10") }
     let(:approval2)   { create(:approval, :user_approval, created_user: current_user, created_at: "2019-11-11") }
-    let(:approval3)   { create(:approval, :user_approval, created_user: other_user,   created_at: "2019-10-10") }
-    let(:approval4)   { create(:approval, :user_approval, created_user: other_user,   created_at: "2019-11-11") }
 
     subject { Approval.search_approval(current_user: current_user, search_created_at: search_created_at) }
 
@@ -33,20 +31,21 @@ RSpec.describe Approval, type: :model do
 
     context 'with role is admin' do
       let(:current_user) { create(:admin_user) }
+      let(:approval3)    { create(:approval, :user_approval, created_user: other_user,   created_at: "2019-12-12") }
 
       context 'and search_created_at is nil' do
         let(:search_created_at) { nil }
 
         it do
-          is_expected.to eq [approval1, approval2, approval3, approval4]
+          is_expected.to eq [approval1, approval2, approval3]
         end
       end
 
       context 'and search_created_at is not nil' do
-        let(:search_created_at) { "2019-10-10" }
+        let(:search_created_at) { "2019-12-12" }
 
         it do
-          is_expected.to eq [approval1, approval3]
+          is_expected.to eq [approval3]
         end
       end
     end
