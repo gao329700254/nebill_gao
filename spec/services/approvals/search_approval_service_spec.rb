ce724cf9f3345e2587450aaba7392637008bc599
Yu Utsugi <yutsugi@cuon.co.jp>
2019-11-15 17:11:37 +0900

    fix

--- spec/services/approvals/search_approval_service_spec.rb
+++ spec/services/approvals/search_approval_service_spec.rb
@@ -3,13 +3,15 @@ require 'rails_helper'
 RSpec.describe Approvals::SearchApprovalService do
   let!(:current_user)     { create(:user) }
   let!(:other_user)       { create(:user) }
-  let!(:approval1)        { create(:approval, :user_approval, created_user_id: current_user.id, name: "abcdefg",
-                                   created_at: "2020-10-10", status: "pending", category: "new_client") }
-  let!(:approval2)        { create(:approval, :user_approval, created_user_id: current_user.id, name: "ghijklm",
-                                   created_at: "2019-10-10", status: "disconfirm", category: "other") }
+  let!(:approval1)        do
+    create(:approval, :user_approval, created_user_id: current_user.id, name: "abcdefg",
+                                      created_at: "2020-10-10", status: "pending", category: "new_client")
+  end
+  let!(:approval2)        do
+    create(:approval, :user_approval, created_user_id: current_user.id, name: "ghijklm",
+                                      created_at: "2019-10-10", status: "disconfirm", category: "other")
+  end
   let!(:approval3)        { create(:approval, :user_approval, created_user_id: other_user.id) }
-
-
 
   subject { Approvals::SearchApprovalService.new(params: search_params, current_user: current_user).execute }
 
@@ -36,7 +38,7 @@ RSpec.describe Approvals::SearchApprovalService do
           page:              '',
         }
       end
-      
+
       it 'should be approvals' do
         is_expected.to eq [approval1, approval2]
       end
